26c5d4507a7745e0a3febe7cee5c88766d4cff02
anitagraser <anitagraser@gmx.at>
2013-10-19 13:40:38 +0200

    added ported Hausdorff distance script and model

---
+++ 1.1/scripts/hausdorff_distance_pairwise.py
@@ -0,0 +1,75 @@
+#Definition of inputs and outputs
+#==================================
+##[my scripts]=group
+##input1=vector
+##join_field1=field input1
+##input2=vector
+##join_field2=field input2
+##output=output vector
+
+#Algorithm body
+#==================================
+from qgis.core import *
+from PyQt4.QtCore import *
+from processing.core.VectorWriter import VectorWriter
+from scipy.spatial.distance import cdist
+import numpy as np
+
+
+# "input1" contains the location of the first input layer
+layer = processing.getobject(input1)
+provider = layer.dataProvider()
+features = processing.getfeatures(layer)
+fields = layer.pendingFields().toList()
+join_field1_index = layer.fieldNameIndex(join_field1)
+nFeat = layer.featureCount()
+
+# "output" is the location of the output file
+# we add field for hausdorff distance to the fields from input1
+fields.append( QgsField("HAUSDORFF", QVariant.Double ))
+writer = VectorWriter(output, None, fields, provider.geometryType(), layer.crs() )
+
+inFeat = QgsFeature()
+inFeat2 = QgsFeature()
+outFeat = QgsFeature()
+inGeom = QgsGeometry()
+
+
+
+# Create output vector layer with additional attribute
+for inFeat  in features:
+    inGeom = inFeat.geometry()
+    atMap = inFeat.attributes()
+    join_value1 = str(atMap[join_field1_index])
+
+    layer2 = processing.getobject(input2)
+    features2 = processing.getfeatures(layer2)
+    join_field2_index = layer2.fieldNameIndex(join_field2)
+
+    distances = []
+    for inFeat2 in features2:
+        inGeom2 = inFeat2.geometry()
+        atMap2 = inFeat2.attributes()
+        join_value2 = str(atMap2[join_field2_index])
+        if join_value1 != join_value2:
+            continue
+        # calculate distances between inGeom and inGeom2
+        D = cdist(inGeom.asPolyline(),inGeom2.asPolyline(),'euclidean')
+        H1 = np.max(np.min(D, axis=1))
+        H2 = np.max(np.min(D, axis=0))
+        distances.append( max(H1,H2) )
+        # repeat the calculation in reverse order
+        D = cdist(inGeom2.asPolyline(),inGeom.asPolyline(),'euclidean')
+        H1 = np.max(np.min(D, axis=1))
+        H2 = np.max(np.min(D, axis=0))
+        distances.append( max(H1,H2) )
+
+    hausdorff = max(distances)
+    outFeat.setGeometry( inGeom )
+    atMap = inFeat.attributes()
+    atMap.append(float(hausdorff))
+    outFeat.setAttributes( atMap )
+    writer.addFeature( outFeat )
+
+del writer
+
--- README.md
+++ README.md
@@ -4,6 +4,16 @@ QGIS Processing Tool Collection
 This collection of Processing (formerly Sextante) tools is first structured by Processing version, 
 then by topic, then by subfolders.
 
+Some tools depend on scipy. If you use the OSGeo4W installer, install python-scipy from the lib section in Advanced installation.
+
+To install tools developed for Processing 1.1 and QGIS 2.0, copy them into
+
+```
+.qgis2/processing/models
+.qgis2/processing/scripts
+```
+
+
 To install the tools developed for Sextante 1.0.8 and QGIS 1.8, copy them into the appropriate Sextante subfolders which you can find in 
 
 ```
@@ -11,4 +21,5 @@ To install the tools developed for Sextante 1.0.8 and QGIS 1.8, copy them into t
 ~/.qgis/sextante/scripts
 ~/.qgis/python/plugins/sextante/ftools
 ```
+
 
