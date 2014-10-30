5c84a4cf50ea2989da9795751772e10035bdbd84
anitagraser <anitagraser@gmx.at>
2014-10-30 17:42:38 -0400

    new global and improved local morans

--- 2.4/scripts/local_morans.py
+++ 2.4/scripts/local_morans.py
@@ -1,6 +1,7 @@
 ##Spatial statistics=group
 ##input=vector
 ##field=field input
+##contiguity=string queen
 ##morans_output=output vector
 
 import pysal 
@@ -10,27 +11,46 @@ from processing.core.VectorWriter import VectorWriter
 from qgis.core import *
 from PyQt4.QtCore import *
 
+# np.random.seed(10)
+
+field = field[0:10] # try to handle Shapefile field length limit
+
 layer = processing.getObject(input)
 provider = layer.dataProvider()
 fields = provider.fields()
 fields.append(QgsField('MORANS_P', QVariant.Double))
+fields.append(QgsField('MORANS_Z', QVariant.Double))
 fields.append(QgsField('MORANS_Q', QVariant.Int))
+fields.append(QgsField('MORANS_I', QVariant.Double))
 writer = VectorWriter(morans_output, None,fields, provider.geometryType(), layer.crs() )
 
-w=pysal.rook_from_shapefile(input)
+if contiguity == 'queen':
+    print 'INFO: Local Moran\'s using queen contiguity'
+    w=pysal.queen_from_shapefile(input)
+else:
+    print 'INFO: Local Moran\'s using rook contiguity'
+    w=pysal.rook_from_shapefile(input)
+
 f = pysal.open(pysal.examples.get_path(input.replace('.shp','.dbf')))
 y=np.array(f.by_col[str(field)])
-lm = pysal.Moran_Local(y,w)
-print lm.p_sim
-print lm.q
-# github.com/pysal/pysal/blob/master/pysal/esda/moran.py
+lm = pysal.Moran_Local(y,w,transformation = "r", permutations = 999)
+
+# http://pysal.readthedocs.org/en/latest/library/esda/moran.html?highlight=local%20moran#pysal.esda.moran.Moran_Local
 # values indicate quadrat location 1 HH,  2 LH,  3 LL,  4 HL
+
 # http://www.biomedware.com/files/documentation/spacestat/Statistics/LM/Results/Interpreting_univariate_Local_Moran_statistics.htm
 # category - scatter plot quadrant - autocorrelation - interpretation
 # high-high - upper right (red) - positive - Cluster - "I'm high and my neighbors are high."
 # high-low - lower right (pink) - negative - Outlier - "I'm a high outlier among low neighbors."
 # low-low - lower left (med. blue) - positive - Cluster - "I'm low and my neighbors are low."
 # low-high - upper left (light blue) - negative - Outlier - "I'm a low outlier among high neighbors."
+
+# http://help.arcgis.com/en/arcgisdesktop/10.0/help/index.html#/What_is_a_z_score_What_is_a_p_value/005p00000006000000/
+# z-score (Standard Deviations) | p-value (Probability) | Confidence level
+#     < -1.65 or > +1.65        |        < 0.10         |       90%
+#     < -1.96 or > +1.96        |        < 0.05         |       95%
+#     < -2.58 or > +2.58        |        < 0.01         |       99%
+
 
 outFeat = QgsFeature()
 i = 0
@@ -39,7 +59,9 @@ for inFeat in processing.features(layer):
     outFeat.setGeometry(inGeom)
     attrs = inFeat.attributes()
     attrs.append(float(lm.p_sim[i]))
+    attrs.append(float(lm.z_sim[i]))
     attrs.append(int(lm.q[i]))
+    attrs.append(float(lm.Is[i]))
     outFeat.setAttributes(attrs)
     writer.addFeature(outFeat)
     i+=1
---
+++ 2.4/scripts/morans.py
@@ -0,0 +1,35 @@
+##Spatial statistics=group
+##input=vector
+##field=field input
+##contiguity=string queen
+
+import pysal 
+import numpy as np
+import processing 
+from processing.core.VectorWriter import VectorWriter
+from qgis.core import *
+from PyQt4.QtCore import *
+
+field = field[0:10] # try to handle Shapefile field length limit
+
+if contiguity == 'queen':
+    print 'INFO: Global Moran\'s using queen contiguity'
+    w=pysal.queen_from_shapefile(input)
+else:
+    print 'INFO: Global Moran\'s using rook contiguity'
+    w=pysal.rook_from_shapefile(input)
+    
+f = pysal.open(pysal.examples.get_path(input.replace('.shp','.dbf')))
+y=np.array(f.by_col[str(field)])
+m = pysal.Moran(y,w,transformation = "r", permutations = 999)
+
+print "Moran's I: %f" % (m.I)
+print "INFO: Moran's I values range from -1 (indicating perfect dispersion) to +1 (perfect correlation). A zero value indicates a random spatial pattern."
+print "p_norm: %f" % (m.p_norm)
+print "p_rand: %f" % (m.p_rand)
+print "p_sim: %f" % (m.p_sim)
+print "INFO: p values smaller than 0.05 indicate spatial autocorrelation that is significant at the 5% level."
+print "z_norm: %f" % (m.z_norm)
+print "z_rand: %f" % (m.z_rand)
+print "z_sim: %f" % (m.z_sim)
+print "INFO: z values greater than 1.96 or smaller than -1.96 indicate spatial autocorrelation that is significant at the 5% level."
